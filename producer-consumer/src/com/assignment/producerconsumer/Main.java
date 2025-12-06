ea1cabc384268b9463f82d187a4cfaa357b16f38
ShreyAC962 <97959780+ShreyAC962@users.noreply.github.com>
2025-12-05 17:17:58 -0800

    Implemented the code for Main.java

--- producer-consumer/src/com/assignment/producerconsumer/Main.java
+++ producer-consumer/src/com/assignment/producerconsumer/Main.java
@@ -1,17 +1,31 @@
 package com.assignment.producerconsumer;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.ArrayList;
+import java.util.List;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+/**
+ * Main demonstrates the complete Producer-Consumer synchronized workflow.
+ */
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+
+        SharedQueue queue = new SharedQueue(5);
+
+        List<Integer> source = List.of(1, 2, 3, 4, 5);
+        List<Integer> destination = new ArrayList<>();
+
+        Producer producer = new Producer(queue, source);
+        Consumer consumer = new Consumer(queue, destination, source.size());
+
+        Thread t1 = new Thread(producer);
+        Thread t2 = new Thread(consumer);
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+
+        System.out.println("Final destination: " + destination);
     }
 }
